87586a0a446a4220317e2d68e8fae94fb9f9088b
goshin-56 <102846714+goshin->
2022-04-18 10:59:04 +0000

    [Add]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,15 @@ class BooksController < ApplicationController
     @book=Book.new
   end
 
+  def create
+    book=Book.new(book_params)
+    book.user_id=current_user.id
+    book.save
+    redirect_to books_path
+  end
+
   def index
+    @book=Book.new
   end
 
   def show
@@ -11,5 +19,11 @@ class BooksController < ApplicationController
 
   def edit
 
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 class UsersController < ApplicationController
   def index
+
   end
 
   def show
+    @book=Book.new
+    @user=User.find(params[:id])
+    @books=@user.books
   end
 
   def edit
+    @user=current_user
+  end
+
+  def update
+    @user=current_user
+    @user.update(user_params)
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
